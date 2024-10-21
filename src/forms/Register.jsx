8bf382fba91b51c3e13a169574493859bf95d118
Brian Steve Pila <hahahaungguy@gmail.com>
2024-10-21 22:23:56 +0800

    Things Added: - Login/Register - No backend though :<

---
+++ src/forms/Register.jsx
@@ -0,0 +1,248 @@
+import '../App.css'
+import { Box, TextField, Button, Typography, Checkbox, FormControlLabel } from "@mui/material";
+import { Link } from "react-router-dom";
+import logotrans from '../assets/logotrans.png';
+import loginbg from '../assets/loginbg.jpg';
+
+export default function Register() {
+    return (
+        <Box
+            component={"section"}
+            sx={{
+                position: 'relative',
+                width: '100svw',
+                height: '100svh',
+                overflow: 'hidden',
+            }}
+        >
+            <Box
+                sx={{
+                    backgroundImage: `url(${loginbg})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    filter: 'blur(3px) brightness(0.3)',
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    zIndex: -1,
+                }}
+            />
+            <Box
+                component={"div"}
+                bgcolor="white"
+                p={4}
+                borderRadius={4}
+                boxShadow={3}
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                width="90%"
+                maxWidth="400px"
+                sx={{ position: 'relative', zIndex: 1, margin: 'auto', top: '50%', transform: 'translateY(-50%)' }}
+            >
+                <Box component={"img"} src={logotrans} alt="logo" width={"100px"} height={"100px"} mb={2} />
+                <Box component={"header"} mb={2}>
+                    <Typography variant="h4" color="black" fontWeight={"bold"}>Register</Typography>
+                </Box>
+                <Box component={"form"} action="/register" method="post" display="flex" flexDirection="column" width="100%">
+                    <TextField
+                        id="name"
+                        label="Name"
+                        variant="outlined"
+                        fullWidth
+                        margin="dense"
+                        required
+                        InputProps={{ style: { backgroundColor: 'white' } }}
+                        sx={{
+                            '& .MuiOutlinedInput-root': {
+                                '& fieldset': {
+                                    borderColor: 'black',
+                                },
+                                '&:hover fieldset': {
+                                    borderColor: 'black',
+                                },
+                                '&.Mui-focused fieldset': {
+                                    borderColor: 'black',
+                                },
+                                '&.Mui-focused .MuiOutlinedInput-input': {
+                                    color: 'black',
+                                },
+                            },
+                            '& .MuiInputLabel-root.Mui-focused': {
+                                color: 'black',
+                            },
+                        }}
+                    />
+                    <TextField
+                        id="email-address"
+                        label="Email Address"
+                        variant="outlined"
+                        fullWidth
+                        margin="dense"
+                        required
+                        InputProps={{ style: { backgroundColor: 'white' } }}
+                        sx={{
+                            '& .MuiOutlinedInput-root': {
+                                '& fieldset': {
+                                    borderColor: 'black',
+                                },
+                                '&:hover fieldset': {
+                                    borderColor: 'black',
+                                },
+                                '&.Mui-focused fieldset': {
+                                    borderColor: 'black',
+                                },
+                                '&.Mui-focused .MuiOutlinedInput-input': {
+                                    color: 'black',
+                                },
+                            },
+                            '& .MuiInputLabel-root.Mui-focused': {
+                                color: 'black',
+                            },
+                        }}
+                    />
+                    <TextField
+                        id="password"
+                        label="Password"
+                        type="password"
+                        variant="outlined"
+                        fullWidth
+                        margin="dense"
+                        required
+                        InputProps={{ style: { backgroundColor: 'white' } }}
+                        sx={{
+                            '& .MuiOutlinedInput-root': {
+                                '& fieldset': {
+                                    borderColor: 'black',
+                                },
+                                '&:hover fieldset': {
+                                    borderColor: 'black',
+                                },
+                                '&.Mui-focused fieldset': {
+                                    borderColor: 'black',
+                                },
+                                '&.Mui-focused .MuiOutlinedInput-input': {
+                                    color: 'black',
+                                },
+                            },
+                            '& .MuiInputLabel-root.Mui-focused': {
+                                color: 'black',
+                            },
+                        }}
+                    />
+                    <TextField
+                        id="confirm-password"
+                        label="Confirm Password"
+                        type="password"
+                        variant="outlined"
+                        fullWidth
+                        margin="dense"
+                        required
+                        InputProps={{ style: { backgroundColor: 'white' } }}
+                        sx={{
+                            '& .MuiOutlinedInput-root': {
+                                '& fieldset': {
+                                    borderColor: 'black',
+                                },
+                                '&:hover fieldset': {
+                                    borderColor: 'black',
+                                },
+                                '&.Mui-focused fieldset': {
+                                    borderColor: 'black',
+                                },
+                                '&.Mui-focused .MuiOutlinedInput-input': {
+                                    color: 'black',
+                                },
+                            },
+                            '& .MuiInputLabel-root.Mui-focused': {
+                                color: 'black',
+                            },
+                        }}
+                    />
+                    <TextField
+                        id="phone-number"
+                        label="Phone Number"
+                        variant="outlined"
+                        fullWidth
+                        margin="dense"
+                        required
+                        InputProps={{ style: { backgroundColor: 'white' } }}
+                        sx={{
+                            '& .MuiOutlinedInput-root': {
+                                '& fieldset': {
+                                    borderColor: 'black',
+                                },
+                                '&:hover fieldset': {
+                                    borderColor: 'black',
+                                },
+                                '&.Mui-focused fieldset': {
+                                    borderColor: 'black',
+                                },
+                                '&.Mui-focused .MuiOutlinedInput-input': {
+                                    color: 'black',
+                                },
+                            },
+                            '& .MuiInputLabel-root.Mui-focused': {
+                                color: 'black',
+                            },
+                        }}
+                    />
+                    <TextField
+                        id="date-of-birth"
+                        label="Date of Birth"
+                        type="date"
+                        variant="outlined"
+                        fullWidth
+                        margin="dense"
+                        required
+                        InputLabelProps={{ shrink: true }}
+                        InputProps={{ style: { backgroundColor: 'white' } }}
+                        sx={{
+                            '& .MuiOutlinedInput-root': {
+                                '& fieldset': {
+                                    borderColor: 'black',
+                                },
+                                '&:hover fieldset': {
+                                    borderColor: 'black',
+                                },
+                                '&.Mui-focused fieldset': {
+                                    borderColor: 'black',
+                                },
+                                '&.Mui-focused .MuiOutlinedInput-input': {
+                                    color: 'black',
+                                },
+                            },
+                            '& .MuiInputLabel-root.Mui-focused': {
+                                color: 'black',
+                            },
+                        }}
+                    />
+                    <FormControlLabel
+                        control={<Checkbox required />}
+                        label="Agree to Terms and Conditions"
+                        sx={{ alignSelf: 'flex-start', marginTop: 2 }}
+                    />
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        fullWidth
+                        sx={{
+                            backgroundColor: 'black',
+                            '&:hover': {
+                                backgroundColor: 'black',
+                            },
+                        }}
+                    >
+                        Register
+                    </Button>
+                </Box>
+                <Typography variant="body2" mt={2} color={"black"}>
+                    Already have an account? <Link to="/login">Login here</Link>
+                </Typography>
+            </Box>
+        </Box>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Login from './forms/login'
+import Register from './forms/Register'
+import './index.css'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/register" element={<Register/>} />
+            {/*    <Route path="/lyrics/singer1" element={<App colorr={"#ADD8E6"} textInputHide={false} />} />*/}
+            {/*    <Route path="/lyrics/singer2" element={<App colorr={"#FF77FF"} textInputHide={false} />} />*/}
+            {/*    <Route path="/lyrics/singer3" element={<App colorr={"#FF9999"} textInputHide={false} />} />*/}
+            {/*    <Route path="/lyrics/singer4" element={<App colorr={"#FFD580"} textInputHide={false} />} />*/}
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>
+)
